c5633b2d29fd987348a9163e70932720a6ffab14
M4mu626 <115762471+M4mu626@users.noreply.github.com>
2022-12-27 15:40:04 -0500

    Add files via upload

---
+++ CampusBootCamp/src/com/tema1/Ejercicio1.java
@@ -0,0 +1,34 @@
+package com.tema1;
+/*
+Para este primer ejercicio tendréis que realizar lo siguiente:
+Crea un proyecto de Java desde 0
+Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2 = 25;
+        System.out.println(variable2);
+        int variable3 = 50;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+        float variable5 = 150.5f;
+        System.out.println(variable5);
+        double variable6 = 200.58d;
+        System.out.println(variable6);
+        boolean variable7 = true;
+        System.out.println(variable7);
+        boolean variable8 = false;
+        System.out.println(variable8);
+        char variable9 = 'o';
+        System.out.println(variable9);
+        String variable10 = "fin de los tipos";
+        System.out.println(variable10);
+    }
+}
